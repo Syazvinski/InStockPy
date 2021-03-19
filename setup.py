5b34455443ff8b22a674da6b1369ad66667bbf1b
Stephan Yazvinski <syazvinski@gmail.com>
2021-03-19 12:11:29 -0400

    More bug fixes

--- setup.py
+++ setup.py
@@ -8,11 +8,14 @@ setup(
   author = 'Stephan Yazvinski',                   # Type in your name
   author_email = 'syazivnski@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/Syazvinski/In-Stock-Bot',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/Syazvinski/In-Stock-Py/archive/01.tar.gz',    # I explain this later on
   keywords = ['In', 'Stock', 'Checker','Proxy','Selenium','In Stock'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
+          'selenium',
+          'colorama',
+          'playsound',
+          'chromedriver-py',
+          'webdriver_manager',
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
