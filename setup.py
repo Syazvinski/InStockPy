a29cdaf05fae18b518a88dc40e474a3681b82e8e
Stephan Yazvinski <syazvinski@gmail.com>
2021-03-19 21:57:08 -0400

    Setup.py change

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from distutils.core import setup
 
 # read the contents of your README file
 from os import path
+from io import open
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
