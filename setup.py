42ac6b490343275cbda7a7d8d15593f5d92e54c0
Stephan Yazvinski <syazvinski@gmail.com>
2021-03-21 10:35:01 -0400

    Setup.py an Reame Updates

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
   author = 'Stephan Yazvinski',                   # Type in your name
   author_email = 'syazivnski@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/Syazvinski/InStockPy',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/Syazvinski/In-Stock-Py/archive/refs/tags/0.1.6.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/Syazvinski/In-Stock-Py/archive/refs/tags/0.1.5.tar.gz',    # I explain this later on
   keywords = ['In', 'Stock', 'Checker','Proxy','Selenium','In Stock'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'selenium',
