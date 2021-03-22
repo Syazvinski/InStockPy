16970a16547cc3f9687d37e42f8b03a314e63386
Stephan Yazvinski <syazvinski@gmail.com>
2021-03-21 22:39:55 -0400

    Revert "Revert "version update""
    This reverts commit 52cc4b557d33d8c8f019c4b7b12981a3ab6edc20.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ from io import open
 setup(
   name = 'InStockPy',         # How you named your package folder (MyLib)
   packages = ['InStockPy'],   # Chose the same as "name"
-  version = '0.2.1',      # Start with a small number and increase it with every change you make
+  version = '0.2.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Checks if an item is in stock at any link based off of specified keywords using selenium and proxys.',   # Give a short description about your library
   #long_description=long_description,
@@ -21,7 +21,7 @@ setup(
   author = 'Stephan Yazvinski',                                                                                                                           # Type in your name
   author_email = 'syazivnski@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/Syazvinski/InStockPy',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/Syazvinski/InStockPy/archive/refs/tags/0.1.9.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/Syazvinski/InStockPy/archive/refs/tags/0.2.2tar.gz',    # I explain this later on
   keywords = ['In', 'Stock', 'Checker','Proxy','Selenium','In Stock','nvidia','bot','scalper'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'selenium',
