d15deb6a6728c6850b2106afb8d1ee30ae3ff633
Stephan Yazvinski <syazvinski@gmail.com>
2021-03-21 21:34:48 -0400

    Readme Change

--- setup.py
+++ setup.py
@@ -3,25 +3,25 @@ from distutils.core import setup
 # read the contents of your README file
 from os import path
 from io import open
-try:
-  this_directory = path.abspath(path.dirname(__file__))
-  with open(path.join(this_directory, 'README.rst'), encoding='utf-8') as f:
-      long_description = f.read()
-except:
-  long_description = "Failure"
+#try:
+  #this_directory = path.abspath(path.dirname(__file__))
+  #with open(path.join(this_directory, 'README.rst'), encoding='utf-8') as f:
+      #long_description = f.read()
+#except:
+  #long_description = "Failure"
 
 setup(
   name = 'InStockPy',         # How you named your package folder (MyLib)
   packages = ['InStockPy'],   # Chose the same as "name"
-  version = '0.1.8',      # Start with a small number and increase it with every change you make
+  version = '0.1.9',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Checks if an item is in stock at any link based off of specified keywords using selenium and proxys.',   # Give a short description about your library
-  long_description=long_description,
-  long_description_content_type='text/markdown',
+  #long_description=long_description,
+  #long_description_content_type='text/markdown',
   author = 'Stephan Yazvinski',                                                                                                                           # Type in your name
   author_email = 'syazivnski@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/Syazvinski/InStockPy',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/Syazvinski/InStockPy/archive/refs/tags/0.1.8.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/Syazvinski/InStockPy/archive/refs/tags/0.1.9.tar.gz',    # I explain this later on
   keywords = ['In', 'Stock', 'Checker','Proxy','Selenium','In Stock','nvidia','bot','scalper'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'selenium',
